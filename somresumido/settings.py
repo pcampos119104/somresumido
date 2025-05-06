f07702e524e4f3ac77592b69821f6639dcfa10b4
pcampos <pcampos119104@gmail.com>
2025-05-06 15:41:18 -0300

    adicionado whitenoise

--- somresumido/settings.py
+++ somresumido/settings.py
@@ -137,7 +137,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'somresumido/static']
+
+STORAGES = {
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
